9056dca0077fc841c854ec7271f7d231f90265c0
Ayan Yenbekbay <ayan.yenb@gmail.com>
2015-06-17 10:14:35 +0600

    Initial Commit

---
+++ Day 4 Photo Bombers App/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Day 4 Photo Bombers App
+//
+//  Created by Tanya Pak on 17.06.15.
+//  Copyright (c) 2015 Tanya Pak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Day 4 Photo Bombers App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day 4 Photo Bombers App
+//
+//  Created by Tanya Pak on 17.06.15.
+//  Copyright (c) 2015 Tanya Pak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
